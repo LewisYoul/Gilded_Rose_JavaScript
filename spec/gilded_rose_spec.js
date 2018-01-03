0d3e177d82ff705cfe8066c559c177e8618cb5a3
Lewis <lewisyoul@gmail.com>
2018-01-03 17:53:44 +0000

    create tests for functionality of updateQuality function

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -8,18 +8,21 @@ describe("Gilded Rose", function() {
   beforeEach(function() {
     items = []
     items.push(new Item('+5 Dexterity Vest', 10, 20));
-    items.push(new Item('Aged Brie', 2, 0));
-    items.push(new Item('Elixir of the Mongoose', 5, 7));
-    items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-    items.push(new Item('Sulfuras, Hand of Ragnaros', -1, 80));
-    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49));
-    items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49));
     shop = new Shop(items)
   });
 
   it("checks that this.items in an instance of Array", function() {
     expect(shop.items instanceof Array).toEqual(true)
+  });
+
+  it("reduces the sellIn value of the first item in the array", function() {
+    shop.updateQuality()
+    expect(shop.items[0].sellIn).toEqual(9)
+  });
+
+  it("reduces the quality value of the first item in the array", function() {
+    shop.updateQuality()
+    expect(shop.items[0].quality).toEqual(19)
   });
 
   it("should equal 4", function() {
