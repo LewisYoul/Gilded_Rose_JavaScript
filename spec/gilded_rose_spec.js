5c23c11cfe4e616dae5c76c35416121e3471a4fd
Lewis <lewisyoul@gmail.com>
2018-01-03 16:49:11 +0000

    first passing test
    test to ensure jasmine is working, 2 + 2 = 4

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,13 @@
 describe("Gilded Rose", function() {
+  // 
+  // it("should foo", function() {
+  //   const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  //   const items = gildedRose.updateQuality();
+  //   expect(items[0].name).toEqual("fixme");
+  // });
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+  it("should equal 4", function() {
+    expect(2 + 2).toEqual(4);
   });
 
 });
