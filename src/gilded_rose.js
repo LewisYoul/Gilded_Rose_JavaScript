f67760efe165b37a7602f181f40368822ef462c6
Lewis <lewisyoul@gmail.com>
2018-01-04 18:48:54 +0000

    comment code

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -37,36 +37,35 @@ class Shop {
       if (item.name != self.unique.brie && item.name != self.unique.pass && item.name != self.unique.sulfuras && item.quality > self.MIN_QUALITY) {
         self._decreaseQuality(item, self.INCREMENT);
       } else {
-        // next line increases quality by 1 because remaining items are all unique (no check for sulfaras?)
+        // next line increases quality by 1 because remaining items are all unique (no check for sulfuras?)
         if (item.quality < self.MAX_QUALITY) {
           self._increaseQuality(item, self.INCREMENT)
           //  if the item is a pass and its sellIn is < 11 increase quality
           if (item.name == self.unique.pass && item.sellIn < 11 && item.quality < self.MAX_QUALITY) {
             self._increaseQuality(item, self.INCREMENT)
-            // if pass sells in 5 days or less increase quality
+            // if pass, sells in 5 days or less increase quality
             if (item.sellIn < 6 && item.quality < self.MAX_QUALITY) {
               self._increaseQuality(item, self.INCREMENT)
             }
           }
         }
       }
+      // reduces the sellIn value if the item is not sulfuras
       if (item.name != self.unique.sulfuras) {
         item.sellIn -= 1;
       }
+      // this section decreases the quality by 1 extra if the item is passed its sell by date
       if (item.sellIn < self.SELL_BY_DATE) {
         if (item.name != self.unique.brie) {
           if (item.name != self.unique.pass) {
-            if (item.quality > self.MIN_QUALITY) {
-              if (item.name != self.unique.sulfuras) {
+            if (item.quality > self.MIN_QUALITY && item.name != self.unique.sulfuras) {
                 self._decreaseQuality(item, self.INCREMENT);
-              }
             }
-          } else {
-            item.quality -= item.quality;
-          }
-        } else {
-          if (item.quality < self.MAX_QUALITY) {
+          } else if (item.quality < self.MAX_QUALITY) {
             self._increaseQuality(item, self.INCREMENT)
+          } else {
+            // set the item quality to zero
+            item.quality -= item.quality;
           }
         }
       }
