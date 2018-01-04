33e4adc145e269c87e0bf265661ac8ab5e6cfa3b
Lewis <lewisyoul@gmail.com>
2018-01-04 10:08:39 +0000

    extract beforeEach to SpecHelper

--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -1,15 +1,12 @@
-beforeEach(function () {
-  jasmine.addMatchers({
-    toBePlaying: function () {
-      return {
-        compare: function (actual, expected) {
-          var player = actual;
-
-          return {
-            pass: player.currentlyPlayingSong === expected && player.isPlaying
-          };
-        }
-      };
-    }
-  });
+beforeEach(function() {
+  items = []
+  items.push(new Item('+5 Dexterity Vest', 10, 20));
+  items.push(new Item('Aged Brie', 2, 0));
+  items.push(new Item('Elixir of the Mongoose', 5, 7));
+  items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
+  items.push(new Item('Sulfuras, Hand of Ragnaros', -1, 80));
+  items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
+  items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49));
+  items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49));
+  shop = new Shop(items)
 });
