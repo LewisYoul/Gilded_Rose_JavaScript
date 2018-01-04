dbf3109d6435d5e73b625775f20d8fab5fcadd1b
Lewis <lewisyoul@gmail.com>
2018-01-04 16:10:52 +0000

    implement hash for unique items
    extract unique item names into a hash that is then referenced

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -9,19 +9,24 @@ class Item {
 class Shop {
   constructor(items=[]){
     this.items = items;
+    this.unique = {
+      brie: 'Aged Brie',
+      pass: 'Backstage passes to a TAFKAL80ETC concert',
+      sulfuras: 'Sulfuras, Hand of Ragnaros'
+    }
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name != this.unique.brie && this.items[i].name != this.unique.pass) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name != this.unique.sulfuras) {
             this.items[i].quality -= 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality += 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name == this.unique.pass) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality += 1;
@@ -35,14 +40,14 @@ class Shop {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name != this.unique.sulfuras) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name != this.unique.brie) {
+          if (this.items[i].name != this.unique.pass) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name != this.unique.sulfuras) {
                 this.items[i].quality  -= 1;
               }
             }
