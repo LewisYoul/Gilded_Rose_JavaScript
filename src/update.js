ef305e2efc4083b53cdd6ed2b78fb7b7632c98ae
Lewis <lewisyoul@gmail.com>
2018-01-05 12:25:39 +0000

    extract reduction of quality/sellIn to private methods

--- src/update.js
+++ src/update.js
@@ -3,7 +3,17 @@ class Update {
   }
 
   updateGeneric(item) {
+    this._reduceQualityByOne(item)
+    this._reduceSellInByOne(item)
+  }
+
+  // PRIVATE
+
+  _reduceQualityByOne(item) {
     item.quality -= 1
+  }
+
+  _reduceSellInByOne(item) {
     item.sellIn -= 1
   }
 }
