85621701d9583c8dce63ad163da9f8c72cf729b8
Lewis <lewisyoul@gmail.com>
2018-01-05 10:19:08 +0000

    create MockItem class

--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -1,13 +1,21 @@
+class MockItem {
+  constructor(name, sellIn, quality){
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
 beforeEach(function() {
   items = []
-  items.push(new Item('+5 Dexterity Vest', 5, 20));
-  items.push(new Item('Aged Brie', 2, 0));
-  items.push(new Item('Elixir of the Mongoose', 5, 7));
-  items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-  items.push(new Item('Sulfuras, Hand of Ragnaros', -1, 80));
-  items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-  items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49));
-  items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49));
+  items.push(new MockItem('+5 Dexterity Vest', 5, 20));
+  items.push(new MockItem('Aged Brie', 2, 0));
+  items.push(new MockItem('Elixir of the Mongoose', 5, 7));
+  items.push(new MockItem('Sulfuras, Hand of Ragnaros', 0, 80));
+  items.push(new MockItem('Sulfuras, Hand of Ragnaros', -1, 80));
+  items.push(new MockItem('Backstage passes to a TAFKAL80ETC concert', 15, 20));
+  items.push(new MockItem('Backstage passes to a TAFKAL80ETC concert', 10, 49));
+  items.push(new MockItem('Backstage passes to a TAFKAL80ETC concert', 5, 49));
   shop = new Shop(items)
 
 });
---
+++ spec/update_spec.js
@@ -0,0 +1,8 @@
+describe("Update", function() {
+
+  describe("#updateGeneric", function() {
+    it("decreases the quality of the item by one", function() {
+
+    });
+  });
+});
